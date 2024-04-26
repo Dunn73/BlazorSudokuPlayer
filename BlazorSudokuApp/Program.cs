2c7746a3a14b25a951ee0a37b2b61cc8e3974526
Evan Dunn <dunnev941@gmail.com>
2024-04-25 21:05:01 -0400

    LocalStorage and keyboard/mouse working

--- BlazorSudokuApp/Program.cs
+++ BlazorSudokuApp/Program.cs
@@ -9,6 +9,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddBlazoredLocalStorage(config => config.JsonSerializerOptions.IncludeFields = true);  // Set include fields to true to handle tuples
 
 await builder.Build().RunAsync();
