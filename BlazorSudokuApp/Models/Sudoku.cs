d7e19e8a4acd3df1a965d84d3e2f8271756d22c0
Evan Dunn <dunnev941@gmail.com>
2024-04-27 15:41:25 -0400

    Created Notes Feature

---
+++ BlazorSudokuApp/Models/Sudoku.cs
@@ -0,0 +1,20 @@
+namespace Models.Sudoku;
+public class SudokuInfo {
+        public int? Id {get; set;}
+        public string? Puzzle { get; set; }
+        public string? Solution { get; set; }
+        public int? GivenNumbers { get; set; }
+        public List<(int, int, char)>? userInputs {get; set;} = new List<(int, int, char)>();
+        public List<(int, int, List<(char,bool)>)>? Notes {get; set;} = new List<(int, int, List<(char,bool)>)>();
+        public SudokuInfo GenerateEmptySudoku(){
+        var emptySudokuInfo = new SudokuInfo{
+                Id = null,
+                Puzzle = "",
+                Solution = "",
+                GivenNumbers = 0,
+                userInputs = [],
+                Notes = []
+                };
+        return emptySudokuInfo;
+    }
+    }
